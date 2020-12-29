danielgora/openair-hss

commit 439bd26b7181c6fac5c338eb96f0318aaf9ef69b

HSS Rel 14. Final fix (?) for better SQN resync (only one round needed)

src/hss_rel14/src/s6as6d_impl.cpp
@@ -28,6 +28,7 @@
 #include "fdhss.h"
 #include "rapidjson/document.h"
 #include "statshss.h"
+#include "util.h"
 
 #include <iomanip>
 extern "C" {
@@ -300,7 +301,7 @@ int CALRcmd::process( FDMessageRequest *req )
 // receives the CALR command.
    return -1;
 }
- 
+
 // AUIR Request (req) Command member functions
 
 
@@ -673,7 +674,7 @@ int INSDRcmd::process( FDMessageRequest *req )
 // receives the INSDR command.
    return -1;
 }
- 
+
 // DESDR Request (req) Command member functions
 
 
@@ -734,7 +735,7 @@ int DESDRcmd::process( FDMessageRequest *req )
 // receives the DESDR command.
    return -1;
 }
- 
+
 // PUUR Request (req) Command member functions
 
 
@@ -865,7 +866,7 @@ int PUURcmd::process( FDMessageRequest *req )
    ans.send();
    return 0;
 }
- 
+
 // RER Request (req) Command member functions
 
 
@@ -926,7 +927,7 @@ int RERcmd::process( FDMessageRequest *req )
 // receives the RER command.
    return -1;
 }
- 
+
 
 }
 
@@ -2155,7 +2156,7 @@ void AIRProcessor::phase1()
    {
       if(m_plmn_len == 3 )
       {
-         if (! Options::getroamallow()) 
+         if (! Options::getroamallow())
          {
             if (apply_access_restriction ((char*)m_imsi.c_str(), m_plmn_id) != 0)
             {
@@ -2228,14 +2229,24 @@ void AIRProcessor::phase2()
       {
         //We succeeded to verify SQN_MS...
         //Pick a new RAND and store SQN_MS + RAND in the HSS
-        generate_random_cpp (m_vector[0].rand, RAND_LENGTH);
+        //generate_random_cpp (m_vector[0].rand, RAND_LENGTH);
 
         // save the new rand and sqn locally
-        memcpy(m_sec.rand, m_vector[0].rand, sizeof(m_sec.rand));
-        memcpy(m_sec.sqn, sqn, sizeof(m_sec.sqn));
+        // LG: useless ?
+        //memcpy(m_sec.rand, m_vector[0].rand, sizeof(m_sec.rand));
+        //memcpy(m_sec.sqn, sqn, sizeof(m_sec.sqn));
 
+        SqnU64Union eu;
+        SQN_TO_U64(sqn, eu);
+        eu.u64 += 32;
+        U64_TO_SQN(eu, m_sec.sqn);
         free (sqn);
+      } else {
+        SqnU64Union eu;
+        SQN_TO_U64(sqn, eu);
+        std::cerr << "Could not resync " << m_uimsi << " HSS SQN " << eu.u64 << std::endl;
       }
+
    }
 
    for (uint32_t i = 0; i < m_num_vectors; i++)
@@ -2243,6 +2254,8 @@ void AIRProcessor::phase2()
       generate_random_cpp (m_vector[i].rand, RAND_LENGTH);
       generate_vector_cpp (m_sec.opc, m_uimsi, m_sec.key, m_plmn_id, m_sec.sqn, &m_vector[i]);
    }
+
+   memcpy(m_sec.rand, m_vector[0].rand, sizeof(m_sec.rand));
 
    for (uint32_t i = 0; i < m_num_vectors; i++)
    {

src/hss_rel14/hsssec/include/auc.h
@@ -2,9 +2,9 @@
  * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under 
+ * The OpenAirInterface Software Alliance licenses this file to You under
  * the Apache License, Version 2.0  (the "License"); you may not use this file
- * except in compliance with the License.  
+ * except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0

src/hss_rel14/hsssec/src/kdf.c
@@ -2,9 +2,9 @@
  * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under 
+ * The OpenAirInterface Software Alliance licenses this file to You under
  * the Apache License, Version 2.0  (the "License"); you may not use this file
- * except in compliance with the License.  
+ * except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0

commit d35d0615003ea91b2928bbc49b80410089c5f072

Fix segfault with SQN resync failure.
Fix a use after free segfault introduced in commit 439bd26b7181 if
sqn_ms_derive_cpp() fails.

src/hss_rel14/src/s6as6d_impl.cpp
@@ -2242,11 +2242,8 @@ void AIRProcessor::phase2()
         U64_TO_SQN(eu, m_sec.sqn);
         free (sqn);
       } else {
-        SqnU64Union eu;
-        SQN_TO_U64(sqn, eu);
-        std::cerr << "Could not resync " << m_uimsi << " HSS SQN " << eu.u64 << std::endl;
+        std::cerr << "Could not resync uimsi: " << m_uimsi << std::endl;
       }
-
    }
 
    for (uint32_t i = 0; i < m_num_vectors; i++)